010a65ee2107840bfac9ae560b6a2c01f05ae7ce
Minecrafthi1 <mimecrafthi30@gmail.com>
2024-06-30 15:03:34 +0800

    Update supportServer.js

--- src/handlers/linkspanel/supportServer.js
+++ src/handlers/linkspanel/supportServer.js
@@ -45,7 +45,7 @@ module.exports = async (client) => {
                     .addComponents(
                         new Discord.ButtonBuilder()
                             .setLabel("Support Server")
-                            .setURL(client.config.discord.serverInvite)
+                            .setURL("https://discord.gg/52C8MtW54M")
                             .setStyle(Discord.ButtonStyle.Link),
                     );
 
